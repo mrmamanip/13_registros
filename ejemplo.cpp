8648d55cdb67a6ead040277df79a0c6dca6c9d8e
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-18 23:38:06 -0500

    Ejemplo 1: Registro de empleados - Actualización: Incremento del 10% en salario a empleados que hayan superado las 100 unidades

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -9,7 +9,7 @@ struct EMP{
 
 int main (){
     EMP EMPLEADOS[100];
-    int n, sum, mayor, numEMP;
+    int n, sum, mayor, numEMP, increm;
     cout<<"Inserte el numero de empleados: ";
     cin>>n;
     for(int i=0;i<n;i++){
@@ -20,11 +20,15 @@ int main (){
         getline(cin,EMPLEADOS[i].nom);
         cout<<"***Ventas del empleado '"<<EMPLEADOS[i].nom<<"'***"<<endl;
 		for (int j=0; j<12;j++){
-        	cout<<"Venta del mes ("<<j<<"): ";
+        	cout<<"Venta del mes ("<<j+1<<"): ";
             cin>>EMPLEADOS[i].ven[j];
+            if (EMPLEADOS[i].ven[j] > 100){
+            	increm=1;
+			}
     	}
         cout<<"Inserte el salario base del empleado "<<EMPLEADOS[i].nom<<": ";
         cin>>EMPLEADOS[i].sal;
+        EMPLEADOS[i].sal = EMPLEADOS[i].sal * 1.1; 
     }
 	numEMP=0;
     for (int i=0; i<n;i++){
@@ -40,6 +44,7 @@ int main (){
     cout<<"La mayor venta fue del empleado:"<<endl;
     cout<<"Nro: "<<EMPLEADOS[numEMP].num<<endl;
     cout<<"Nombre: "<<EMPLEADOS[numEMP].nom<<endl;
+    cout<<"Salario total del empleado "<<EMPLEADOS[numEMP].nom<<": "<<EMPLEADOS[numEMP].sal;
     
 
     return 0;
