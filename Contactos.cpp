a582e740e9a1ce8307d430f1be1a688b7cf61f3b
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-25 23:50:04 -0500

    Registro de contactos - Actualización: Mejoras gráficas en el apartado de Modificar contactos

--- Contactos.cpp
+++ Contactos.cpp
@@ -18,13 +18,14 @@ void leerContacto(contactoEmail &, string, char, int, correo);
 void imprimeContacto(contactoEmail &);
 
 int main(){
-    int n, op, modifi, mod, modcorreo;
+    int n, op, modifi, mod, modcorreo, opt;
     string nom, user, domain;
     char sex, option;
     int edad;
     correo email;
     contactoEmail cont, lista[100];
     n = 0;
+    opt = 0;
     do{
         system("cls");
         cout<<"Menu de opciones -------------------------"<<endl;
@@ -64,8 +65,11 @@ int main(){
             	cout<<"Inserte el numero de orden del contacto: ";cin>>mod;
                 do{
                 	system("cls");
-					cout<<"Menu de cambios en el contacto #"<<mod<<": ---"<<endl;
-                	cout<<"1. Modificar nombre"<<endl;
+                	cout<<"Menu de cambios en el contacto #"<<mod<<": ---"<<endl<<endl;
+                	cout<<"Datos del contacto:"<<endl;
+                	imprimeContacto(lista[mod-1]);
+                	cout<<endl<<endl;
+					cout<<"1. Modificar nombre"<<endl;
                 	cout<<"2. Modificar sexo"<<endl;
                 	cout<<"3. Modificar edad"<<endl;
                 	cout<<"4. Modificar correo electronico"<<endl;
@@ -75,12 +79,15 @@ int main(){
                 		case 1:
                 			cin.ignore();
                 			cout<<"Inserte nuevo nombre: "; cin>>lista[mod-1].nom;
+                			system("pause");
                 			break;
                 		case 2:
                 			cout<<"Inserte nuevo sexo: "; cin>>lista[mod-1].sex;
+                			system("pause");
                 			break;
                 		case 3:
                 			cout<<"Inserte nueva edad: "; cin>>lista[mod-1].edad;
+                			system("pause");
                 			break;
                 		case 4:
                 			do{
@@ -93,13 +100,16 @@ int main(){
                 				switch(modcorreo){
                 					case 1:
                 						cout<<"Inserte nuevo usuario: ";cin>>lista[mod-1].email.user;
+                						system("pause");
                 						break;
                 					case 2:
                 						cout<<"Inserte nuevo dominio: "; cin>>lista[mod-1].email.domain;
+                						system("pause");
                 					case 0:
                 						break;
                 					default:
                 						cout<<"Opcion no valida!"<<endl;
+                						system("pause");
                 						break;
                 					}
 							} while(modcorreo != 0);
@@ -111,14 +121,17 @@ int main(){
                 	}
                 } while(modifi != 0);
             case 0:
-                cout<<"Esta seguro de salir? (S/N): ";
+                cout<<"Esta seguro de salir? (S/N): ";cin>>option;
+                if (option == 's' || option == 'S'){
+                	opt=1;
+				}
                 break;
             default:
                 cout<<"Opcion no valida!"<<endl;
                 system("pause");
                 break;
         }
-    } while(op != 0);
+    } while(opt != 1);
     return 0;
 }
 
