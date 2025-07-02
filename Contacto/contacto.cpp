07db06eb4a9c8651b77161516674fa7e95a3c497
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-07-02 00:06:28 -0500

    Implementación de funciones para la información del contacto

---
+++ Contacto/contacto.cpp
@@ -0,0 +1,23 @@
+#include "contacto.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void leerContacto(contactoEmail &c, string n, char s, int e, correo em){
+    c.nom = n;
+    c.sex = s;
+    c.edad = e;
+    c.email = em;
+}
+
+void leerCorreo(correo &c, string u, string d){
+    c.user = u;
+    c.domain = d;
+}
+
+void imprimeContacto(contactoEmail &c){
+    cout<<"Nombre: "<<c.nom<<endl;
+    cout<<"Sexo: "<<c.sex<<endl;
+    cout<<"Edad: "<<c.edad<<endl;
+    cout<<"Email: "<<c.email.user<<"@"<<c.email.domain<<endl;
+}
